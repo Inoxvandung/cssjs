c3868c3191c5abc0b22e8a9b759d403db055bf6d
Inoxvandung <95286189+Inoxvandung@users.noreply.github.com>
2022-01-03 21:11:13 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>KHONG LAM MA DOI CO AN :)) : <a href='https://inoxnhanvuong.com/'>https://inoxnhanvuong.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>KHONG LAM MA DOI CO AN :)) : <a href='https://inoxvandung.com/'>https://inoxvandung.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
